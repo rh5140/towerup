bc0ef8b2295e22056ab6cae4b4f528fa6b3b3fbe
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2024-08-19 21:27:29 -0700

    Buttons update

---
+++ gmtk24/Assets/Scripts/Buttons.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Buttons : MonoBehaviour
+{
+    
+    [SerializeField] private TextMeshProUGUI buttonText;
+    private float initialFontSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialFontSize = buttonText.fontSize;
+    }
+
+    public void IncreaseSize() {
+        buttonText.fontSize += 4;
+    }
+
+    public void ResetSize() {
+        buttonText.fontSize = initialFontSize;
+    }
+}
